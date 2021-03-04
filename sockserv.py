284bf8e2780d67e20d43147b96e8fc09521b4402
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-03-04 10:37:21 -0500

    Wrapped server.py in Server class

--- sockserv.py
+++ sockserv.py
@@ -2,73 +2,79 @@ import socket
 import select
 import pickle
 
-HEADER_LENGTH = 10
-IP = "127.0.0.1"
-PORT = 8000
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class Server:
+    HEADER_LENGTH = 10
+    IP = "127.0.0.1"
+    PORT = 8000
 
-server_socket.bind((IP, PORT))
-server_socket.listen()
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((Server.IP, Server.PORT))
+        self.server_socket.listen()
 
-sockets_list = [server_socket]
+        self.sockets_list = [self.server_socket]
+        self.clients = {}
 
-clients = {}
+    def send_pickle(self, message, user, send_sock):
+        content_dict = {'message': message, 'user': user}
+        dict_pick = pickle.dumps(content_dict)
+        pick_mess = bytes(f"{len(dict_pick):<{Server.HEADER_LENGTH}}", "utf-8") + dict_pick
+        send_sock.send(pick_mess)
 
-def send_pickle(message, user, send_sock):
-    content_dict = {'message': message, 'user': user}
-    dict_pick = pickle.dumps(content_dict)
-    pick_mess = bytes(f"{len(dict_pick):<{HEADER_LENGTH}}", "utf-8") + dict_pick
-    send_sock.send(pick_mess)
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(Server.HEADER_LENGTH)
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
+            if not len(message_header):
+                return False
 
-        if not len(message_header):
+            message_length = int(message_header.decode('utf-8').strip())
+            return {"header": message_header,
+            "data": client_socket.recv(message_length)}
+        except:
             return False
 
-        message_length = int(message_header.decode('utf-8').strip())
-        return {"header": message_header,
-        "data": client_socket.recv(message_length)}
-    except:
-        return False
 
+    def listen(self):
+        read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+        for notified_socket in read_sockets:
+            if notified_socket == self.server_socket:
+                client_socket, client_addr = self.server_socket.accept()
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_addr = server_socket.accept()
+                user = self.receive_message(client_socket)
+                if user is False:
+                    continue
 
-            user = receive_message(client_socket)
-            if user is False:
-                continue
+                self.sockets_list.append(client_socket)
 
-            sockets_list.append(client_socket)
+                self.clients[client_socket] = user
 
-            clients[client_socket] = user
+                print(f"accepted new connection from \
+    {client_addr[0]}: {client_addr[1]} username = {user['data'].decode('utf-8')}")
 
-            print(f"accepted new connection from \
-{client_addr[0]}: {client_addr[1]} username = {user['data'].decode('utf-8')}")
+            else:
+                message = self.receive_message(notified_socket)
 
-        else:
-            message = receive_message(notified_socket)
+                if message is False:
+                    print(f"Closed connection from {self.clients[notified_socket]['data'].decode('utf-8')}")
+                    self.sockets_list.remove(notified_socket)
+                    del self.clients[notified_socket]
+                    continue
 
-            if message is False:
-                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
+                user = self.clients[notified_socket]
+                print(f"received message from {user['data'].decode('utf-8')}: {pickle.loads(message['data'])}")
+                for client_socket in self.clients:
+                    if client_socket != notified_socket:
+                        self.send_pickle(pickle.loads(message['data']), user['data'].decode('utf-8'), client_socket)
 
-            user = clients[notified_socket]
-            print(f"received message from {user['data'].decode('utf-8')}: {pickle.loads(message['data'])}")
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    send_pickle(pickle.loads(message['data']), user['data'].decode('utf-8'), client_socket)
+        for notified_socket in exception_sockets:
+            self.sockets_list.remove(notified_socket)
+            del self.clients[notified_socket]
 
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+if __name__ == "__main__":
+    server = Server()
+    while True:
+        server.listen()
